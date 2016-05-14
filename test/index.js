c1b42eaa7c9b2277925276159c02df90bccdae54
Paul OFallon <paul@ofallonfamily.com>
2016-05-14 15:27:26 -0400

    More tests

--- test/index.js
+++ test/index.js
@@ -2,6 +2,8 @@ var fakeRedis = require('fakeredis');
 var expect = require('chai').expect;
 var Tynee = require('../index');
 var express = require('express');
+var bodyParser = require('body-parser');
+var TyneeClient = require('tynee-client');
 
 describe('A Tynee Server', function() {
    
@@ -17,7 +19,7 @@ describe('A Tynee Server', function() {
              name: 'foo',
              version: '1.0.0',
              service: function(m, cb) {
-                 process.nextTime(function() {
+                 process.nextTick(function() {
                      callback(null, 'bar');
                  });
              }
@@ -28,6 +30,80 @@ describe('A Tynee Server', function() {
          });
          
          tynee.publish();
+          
+      });
+      
+      it('should emit an "online" event', function(done) {
+                   
+         var tynee = new Tynee({
+             redisClient: fakeRedis.createClient()
+         });
+         
+         tynee.add({
+             name: 'foo',
+             version: '1.0.0',
+             service: function(m, cb) {
+                 process.nextTick(function() {
+                     callback(null, 'bar');
+                 });
+             }
+         });
+         
+         tynee.on('online', function() {
+             done();
+         });
+         
+         tynee.publish();
+          
+      });
+       
+   });
+   
+   describe('when used as express middleware', function() {
+      
+      it('should handle a request', function(done) {
+         
+         var app = express();
+         
+         var redisClient = fakeRedis.createClient();
+         
+         var tynee = new Tynee({
+             redisClient: redisClient
+         });
+         
+         tynee.add({
+             name: 'foo',
+             version: '1.0.0',
+             service: function(m, cb) {
+                 process.nextTick(function() {
+                     cb(null, 'bar');
+                 });
+             }
+         });
+         
+         app.use(bodyParser.json());
+         app.use(tynee.handler());
+         app.listen(3000).on('listening', function() {
+             tynee.publish(3000);
+         });
+ 
+         tynee.on('ready', function() {
+             
+             var client = new TyneeClient({
+                 redisClient: redisClient,
+                 dependencies: {name: 'foo', version: '1.0.0'}
+             });
+             
+             client.on('ready', function() {
+                 
+                client.invoke({ name: 'foo', body: ''}, function(err, results) {
+                    expect(results.body).to.equal('bar');
+                    done();
+                });
+                
+             });
+             
+         });
           
       });
        
